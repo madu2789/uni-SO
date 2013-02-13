6e2fdd8938b35f719b501ee31583a834b8a89fab
albhilazo@gmail.com <albhilazo@gmail.com@508b31c3-7d50-492a-ad4b-6eb9a79fb966>
2013-02-13 11:00:00 +0000

    Afegit .h temporal, pendent de revisió.

---
+++ trunk/client/client.h
@@ -0,0 +1,41 @@
+/* **********************************************
+*
+* Fitxer de capçalera de Client
+*
+* Daniel Madurell Blanes (is19445)
+* Albert Hilazo Aguilera (se19467)
+*
+* ********************************************** */
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+
+// Includes del Sistema
+#include <unistd.h>			// write i read
+#include <stdio.h>			// sprintf
+#include <stdlib.h>
+#include <string.h>			// strlen, bzero
+#include <dirent.h>			// opendir, readdir...
+#include <sys/stat.h>		// stat, obtenir data modificacio
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
+#include <signal.h>
+
+// Includes propios
+//#include <LinkedList.h>
+
+// Definicion de constantes
+#define ERROR -1
+#define OK 0
+#define MAX 64
+
+
+// Definicion de tipos propios
+
+
+
+//Funciones
+int main();
+
+#endif
