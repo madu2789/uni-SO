08834b65457486441d5b84041450766489f48df3
madu2789 <madu2789@508b31c3-7d50-492a-ad4b-6eb9a79fb966>
2013-02-19 11:01:12 +0000

    restructuracio de codi

--- trunk/utils/LinkedList.c
+++ trunk/utils/LinkedList.c
@@ -11,10 +11,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-//aixo ha danar fora
-char sProvaName[30];
-char sProvaDate[64];
-
 /* STRUCTURE : DATA PART AND A LINK PART */
 struct node {
   char sName[30];
@@ -27,186 +23,172 @@ struct node {
 /*THIS FUNCTION DELETES A NODE */
 void delnode (char sName[30]) {
   struct node *temp, *m;
-	temp=p;
+	temp = p;
 
-  while(temp != NULL){
-    if(strcmp(temp->sName, sName) == 0){
-      if(temp == p){
-        p=temp->next;
+  while (temp != NULL) {
+    if (strcmp(temp->sName, sName) == 0) {
+      if (temp == p) {
+        p = temp->next;
         //free(temp); diria k no cal alliberar pk no arribes a demanar memoria (HIL?)
         return;
-      }else{
-        m->next=temp->next;
+      } else {
+        m->next = temp->next;
         //free(temp);
         return;
       }
-    }else{
-      m=temp;
-      temp= temp->next;
+    } else {
+      m = temp;
+      temp = temp->next;
     }
 	}
-    printf(" ELEMENT %s NOT FOUND!\n", sName);
 }
 
 /*THIS FUNCTION ADDS A NODE AT THE LAST OF LINKED LIST */
 void append (char sName[30], char sTipus[30], char sDate[64]) {
   struct node *temp,*r;
-  temp= (struct node *)malloc(sizeof(struct node));
+  temp = (struct node *)malloc(sizeof(struct node));
 
   strcpy(temp->sName, sName);
   strcpy(temp->sTipus, sTipus);
   strcpy(temp->sDate, sDate);
-  r=(struct node *)p;
+  r = (struct node *)p;
 
   if (p == NULL) {
-    p=temp;
-    p->next =NULL;
-  }else{
-    while( r->next != NULL)
-      r=r->next;
-      r->next =temp;
-      r=temp;
-      r->next=NULL;
+    p = temp;
+    p->next = NULL;
+  } else {
+    while (r->next != NULL)
+      r = r->next;
+      r->next = temp;
+      r = temp;
+      r->next = NULL;
     }
   }
 
 /* ADD A NEW NODE AT BEGINNING  */
 void addbeg (char sName[30], char sTipus[30], char sDate[64]) {
   struct node *temp;
-  temp=(struct node *)malloc(sizeof(struct node));
+  temp =(struct node *)malloc(sizeof(struct node));
 
   strcpy(temp->sName, sName);
   strcpy(temp->sTipus, sTipus);
   strcpy(temp->sDate, sDate);
 
   if (p == NULL) {
-    p=temp;
-    p->next=NULL;
-  }else{
-    temp->next=p;
-    p=temp;
+    p = temp;
+    p->next = NULL;
+  } else {
+    temp->next = p;
+    p = temp;
   }
 }
 
-/*  ADD A NEW NODE AFTER A SPECIFIED NO OF NODES */
+/* ADD A NEW NODE AFTER A SPECIFIED NO OF NODES */
 void addafter (int loc , char sName[30], char sTipus[30], char sDate[64]) {
   int i;
   struct node *temp,*t,*r;
-  r=p;       // here r stores the first location
+  //here r stores the first location
+  r = p;
 
-  if(loc > count()+1 || loc <= 0) {
+  if (loc > count()+1 || loc <= 0) {
     printf("insertion is not possible :\n");
     return;
   }
-  if (loc == 1){
+  if (loc == 1) {
     addbeg(sName, sTipus, sDate);
     return;
-  }else{
-    for(i=1;i<loc;i++){
-      t=r;   // t will be holding previous value
-      r=r->next;
+  } else {
+    for (i=1;i<loc;i++) {
+      t = r;   // t will be holding previous value
+      r = r->next;
     }
-    temp=(struct node *)malloc(sizeof(struct node));
+    temp = (struct node *)malloc(sizeof(struct node));
 
     strcpy(temp->sName, sName);
     strcpy(temp->sTipus, sTipus);
     strcpy(temp->sDate, sDate);
-    t->next=temp;
-    t=temp;
-    t->next=r;
+    t->next = temp;
+    t = temp;
+    t->next = r;
     return;
   }
 }
 
 /* THIS FUNCTION DISPLAYS THE CONTENTS OF THE LINKED LIST */
 void display (struct node *r) {
-  r=p;
-  if(r == NULL) {
+  r = p;
+  if (r == NULL) {
     printf("NO ELEMENT IN THE LIST :\n");
     return;
   }
-  while(r != NULL) {
+  while (r != NULL) {
     printf(" -> %s \t %s \t %s \n", r->sName, r->sTipus, r->sDate );
-    r=r->next;
+    r = r->next;
   }
     printf(" ");
 }
 
 /*Show a A NODE (molt guarro utilitzant variables globals->REFACTORING)*/
-void showNode (int nLocation) {
+int showNode (char sName[30], int nLocation) {
   struct node *temp;
   int i = 0, bTrobat = 0;
   temp=p;
 
-  if (nLocation-1 < count() ) {
-    while(temp != NULL && bTrobat == 0){
-        if(i == nLocation-1){
+    while (temp != NULL && bTrobat == 0) {
+        if (i == nLocation-1) {
           bTrobat = 1;
+          printf(" ELEMENT %s FOUND!\n", temp->sName);
+          strcpy(sName, temp->sName);
         } else {
-          temp= temp->next;
+          temp = temp->next;
         }
         i++;
-        strcpy(sProvaName ,temp->sName);
-        strcpy(sProvaDate, temp->sDate);
-        printf(" ELEMENT %s FOUND!\n", temp->sName);
-    }
-  } else {
-         printf(" ELEMENT DOESN'T EXISTS!\n");
-  }
-  return;
-}
-
-/*Show a A NODE (molt guarro utilitzant variables globals->REFACTORING)*/
-int getDateByName (char sName[30]) {
-  struct node *temp;
-  int bTrobat = 0;
-  temp=p;
-
-    while(temp != NULL && bTrobat == 0){
-        if(strcmp(sName, temp->sName) == 0){
-          bTrobat = 1;
-        } else {
-          temp= temp->next;
-        }
-        strcpy(sProvaDate, temp->sDate);
-        //printf(" ELEMENT %s FOUND!\n", temp->sName);
-    }
-    if(bTrobat == 0){
-      printf(" ELEMENT DOESN'T EXISTS!\n");
-      return -1;
     }
   return 0;
+}
+
+/*Show a A Date NODE By Name*/
+int getDateByName (char sDate[30], char sName[30]) {
+  struct node *temp;
+  int bTrobat = 0;
+  temp = p;
+
+  while(temp != NULL && bTrobat == 0){
+    if(strcmp(sName, temp->sName) == 0){
+      bTrobat = 1;
+      strcpy(sDate, temp->sDate);
+    } else {
+      temp= temp->next;
+    }
+    //printf(" ELEMENT %s FOUND!\n", temp->sName);
+  }
+  return bTrobat;
 }
 
 /*Canvia la sDate segons el sName*/
 int setDateByName (char sName[30], char sDate[64]) {
   struct node *temp;
   int bTrobat = 0;
-  temp=p;
+  temp = p;
 
-    while(temp != NULL && bTrobat == 0){
-        if(strcmp(sName, temp->sName) == 0){
-          bTrobat = 1;
-          strcpy(temp->sDate, sDate);
-        } else {
-          temp= temp->next;
-        }
-        //printf(" ELEMENT %s FOUND!\n", temp->sName);
+  while (temp != NULL && bTrobat == 0) {
+    if (strcmp(sName, temp->sName) == 0) {
+      bTrobat = 1;
+      strcpy(temp->sDate, sDate);
+    } else {
+      temp = temp->next;
     }
-    if(bTrobat == 0){
-      printf(" ELEMENT DOESN'T EXISTS!\n");
-      return -1;
-    }
+  }
   return 0;
 }
 
 //THIS FUNCTION COUNTS THE NUMBER OF ELEMENTS IN THE LIST
 int count (void) {
   struct node *n;
-  int c=0;
-  n=p;
-  while(n!=NULL){
-    n=n->next;
+  int c = 0;
+  n = p;
+  while (n != NULL) {
+    n = n->next;
     c++;
   }
   return(c);
@@ -215,15 +197,15 @@ int count (void) {
 //THIS FUNCTION REVERSES A LINKED LIST
 void reverse (struct node *q) {
   struct node *m,*n,*s;
-  m=q;
-  n=NULL;
-  while(m != NULL){
-    s=n;
-    n=m;
-    m=m->next;
-    n->next=s;
+  m = q;
+  n = NULL;
+  while (m != NULL) {
+    s = n;
+    n = m;
+    m = m->next;
+    n->next = s;
   }
-  p=n;
+  p = n;
 }
 
 /* THIS IS THE MAIN PROGRAM  */
@@ -244,7 +226,7 @@ int main () {
 		printf("PLEASE, ENTER THE NUMBER:\n");
 
 		scanf("%d",&i);
-    switch(i){
+    switch (i) {
       case 1: {
         char sName[30], sTipus[30], sDate[64];
         printf(" PLEASE ENTER sName, sType, sDate :-");
@@ -300,11 +282,11 @@ int main () {
         break;
       }
       case 8: {
-        char sName[30];
+        char sName[30], sDate[64];
         printf("PLEASE ENTER A sName FROM THE LIST :");
         scanf("%s",&sName);
-        getDateByName(sName);
-        printf("%s\n", sProvaDate);
+        getDateByName(sDate, sName);
+        printf("%s\n", sDate);
         break;
       }
       case 9: {
